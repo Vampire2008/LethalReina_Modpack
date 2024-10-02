187d91c25581cd11dbeaaa06aaac97ae76563367
Mikhail Kislenko <citizen.of.atlantis@outlook.com>
2024-10-02 23:13:53 +0400

    Updated script and workflow

--- pack.ps1
+++ pack.ps1
@@ -1,14 +1,18 @@
 param($mod)
 
+if (!(Test-Path "$PSScriptRoot/dist")) {
+    New-Item -Path "$PSScriptRoot/dist" -ItemType Directory
+}
+
 if (!(Test-Path "$PSScriptRoot/$mod")) {
     Write-Error "Mod with name $mod not exists"
     exit 1
 }
 
-$outputFile = "$PSScriptRoot/LethalReina_Modpack_$mod.zip"
+$outputFile = "$PSScriptRoot/dist/LethalReina_Modpack_$mod.zip"
 
 if (Test-Path $outputFile) {
     Remove-Item $outputFile
 }
 
-Compress-Archive -Path ("$PSScriptRoot/$mod/*", "$PSScriptRoot/README.md"") -DestinationPath $outputFile
+Compress-Archive -Path ("$PSScriptRoot/$mod/*") -DestinationPath $outputFile
